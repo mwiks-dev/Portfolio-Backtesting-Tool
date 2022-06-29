4caaf9d043f051cd4a42990d336a41d854fc282f
Maryann <mwikali119@gmail.com>
2022-06-29 11:22:03 +0300

    Retrieved data from NSE website

--- stocks.py
+++ stocks.py
@@ -1,32 +1,23 @@
-import numpy as np
-import pandas as pd
-import pandas_datareader as web
-import plotly.express as px
-import pandas_datareader
+from matplotlib import ticker
+from nsepy import get_history
+from datetime import date
 import datetime
-import pandas_datareader.data as web
+import matplotlib
+from pandas_datareader import data as pdr
+import yfinance as yf
+import pandas as pd
 import plotly.graph_objects as go
 
-start = datetime.datetime(2019, 1, 1)
-end = datetime.datetime(2020, 10, 30)
-AAPL = web.DataReader('AAPL','yahoo',start,end)
-FB = web.DataReader('FB','yahoo',start,end)
-MSFT = web.DataReader('MSFT','yahoo',start,end)
-CRM = web.DataReader('CRM','yahoo',start,end)
+start = datetime.datetime(2022, 1, 1)
+end = datetime.datetime(2022, 1, 31)
 
-##Banchmark SPY for comparision
-SPY = web.DataReader('SPY','yahoo',start,end)
+data = pd.read_html('https://ournifty.com/stock-list-in-nse-fo-futures-and-options.html#:~:text=NSE%20F%26O%20Stock%20List%3A%20%20%20%20SL,%20%201000%20%2052%20more%20rows%20')[0]
+labels = data.loc[[124,126,127]]
+print(data.loc[[124,125,126,127]])
+allocation_pct =[.1,.2,.3,.4]
 
-#Portfolio Asset Allocation. 
-labels = ['AAPL','FB','MSFT','CRM']
-allocation_pct = [.2,.3,.4,.1]
 
-fig = go.Figure(data=[go.Pie(labels=labels, values=allocation_pct)])
-fig.update_layout(title="Portfolio Asset Allocation")
+# print(labels)
+fig = go.Figure(data=[go.Pie(labels=labels, values=allocation_pct) ])
+fig.update_layout(title_text="NSE F&O Stock List")
 fig.show()
-
-# Calculate the cumulative return
-for df in (AAPL,FB,MSFT,CRM,SPY): 
-  df['Cum Return'] = df['Adj Close']/df.iloc[0]['Adj Close']
-
-AAPL.head()
