1e4580efc679ed6bb8ace68343904e6773a45dff
Maryann <mwikali119@gmail.com>
2022-06-30 09:45:09 +0300

    Added user input fields

--- nsehistory_yf.py
+++ nsehistory_yf.py
@@ -1,39 +1,33 @@
 import yfinance as yf
 import pandas as pd
-import datetime
+import datetime as dt
 import pandas_datareader.data as web
-import csv
-import collections
 
-#the start expression is for February 1, 2021
-start = datetime.date(2021,2,1)
- 
-#the end expression is for February 28, 2019
-end = datetime.date(2021,2,28)
+# nse = yf.Ticker('^NSEBANK')
+print("Enter valid SYMBOL")
+nse = yf.Ticker(input(''))
+print("Enter start date in YYYY-MM-DD format")
+s_time = dt.datetime.strptime(input(''), '%Y-%m-%d')
+print("Enter end date in YYYY-MM-DD format")
+e_time = dt.datetime.strptime(input(''), '%Y-%m-%d')
 
-nse = yf.Ticker('^NSEI')
+# print("Enter interval in minutes,days,weeks or months")
+# print('For example if you want to get data for last 30 minutes, enter "30m"')
+# print('For example if you want to get data for last 5 days, enter "5d"')
+# print('For example if you want to get data for last 2 weeks, enter "2w"')
+# print('For example if you want to get data for last 1 month, enter "1mo"')
+# interval = input('')
 
 # nse_history = nse.history(period='max')
 # nse_history = nse.history(start='2022-05-01', end='2022-05-31',interval="30m",actions=False)
-nse_history = nse.history(start='2022-05-01', end='2022-05-31',interval="1d",actions=False,prepost=True)
+nse_history = nse.history(start=s_time, end= e_time,actions=False,prepost=True)
 
 pd.options.display.width = 0
 pd.set_option('display.max_rows',20000)
+pd.set_option('display.max_columns',4)
 print(nse_history)
 
-with open('/home/mwiks-dev/Projects/Personal/Portfolio-Backtest/output.txt') as f:
-    data = list(csv.reader(f))
-
-counter = collections.defaultdict(int)
-for row in data:
-    counter[row[0]] += 1
-
-writer = csv.writer(open('/home/mwiks-dev/Projects/Personal/Portfolio-Backtest/output.csv', 'w'))
-for row in data:
-    if counter[row[0]] >= 4:
-        writer.writerow(row)
-
-
+nse_history.to_csv('output.csv')
 
 # nse_data = yf.download('NSE',start='2021-2-1',end='2021-2-28',auto_adjust=True)
 # print (nse_data)
