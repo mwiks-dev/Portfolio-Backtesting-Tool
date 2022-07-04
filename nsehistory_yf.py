27746f92871c7586bf501b1c52ecb85191e6b812
Maryann <mwikali119@gmail.com>
2022-07-04 12:54:16 +0300

    Find price change and volume change

--- nsehistory_yf.py
+++ nsehistory_yf.py
@@ -1,7 +1,9 @@
 import yfinance as yf
 import pandas as pd
 import datetime as dt
-import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import linregress
 
 # nse = yf.Ticker('^NSEBANK')
 print("Enter valid SYMBOL")
@@ -10,22 +12,56 @@ print("Enter start date in YYYY-MM-DD format")
 s_time = dt.datetime.strptime(input(''), '%Y-%m-%d')
 print("Enter end date in YYYY-MM-DD format")
 e_time = dt.datetime.strptime(input(''), '%Y-%m-%d')
-
-# print("Enter interval in minutes,days,weeks or months")
-# print('For example if you want to get data for last 30 minutes, enter "30m"')
-# print('For example if you want to get data for last 5 days, enter "5d"')
-# print('For example if you want to get data for last 2 weeks, enter "2w"')
-# print('For example if you want to get data for last 1 month, enter "1mo"')
-# interval = input('')
-
 # nse_history = nse.history(period='max')
-# nse_history = nse.history(start='2022-05-01', end='2022-05-31',interval="30m",actions=False)
 nse_history = nse.history(start=s_time, end= e_time,actions=False,prepost=True)
 
 pd.options.display.width = 0
 pd.set_option('display.max_rows',20000)
-pd.set_option('display.max_columns',4)
+pd.set_option('display.max_columns',6)
+# %matplotlib inline
+plt.rcParams['figure.figsize'] = (10, 6)
+plt.ioff()
+nse_history['Price Change'] = nse_history['Close'].rolling(window=30).mean()
+nse_history['Volume Change'] = nse_history['Volume'].rolling(window=30).mean()
+nse_history = nse_history[nse_history['Price Change'].notna()]
+nse_history.to_csv('output.csv')
 print(nse_history)
 
-nse_history.to_csv('output.csv')
+# tickers = pd.read_csv('output.csv')
+# print(tickers)
+# stocksH = (
+#     pd.concat(
+#         [pd.read_csv(f"output.csv", index_col='Date',parse_dates=True)[
+#             'Close'
+#         ].rename(ticker)
+#         for ticker in tickers],
+#         axis=1,
+#         sort=True
+#     )
+# )
+# stocks = stocksH.loc[:,~stocksH.columns.duplicated()]
+# print(stocks)
+# def momentum():
+#     returns = np.log()
+#     x = np.arange(len(returns))
+#     slope, _, rvalue, _, _ = linregress(x, returns)
+#     return ((1 + slope) ** 252) * (rvalue ** 2)
 
+# momentums = stocks.copy(deep=True)
+# for ticker in tickers:
+#     momentums[ticker] = stocks[ticker].rolling(90).apply(momentum, raw=False)
+
+# df = momentums.dropna()
+# # print(df)
+# # plt.figure(figsize=(12,9))
+# # plt.xlabel('Days')
+# # plt.ylabel('Stock Price')
+
+# # bests = momentums.max().sort_values(ascending=False).index[:5]
+# # for best in bests:
+# #     end = momentums[best].index.get_loc(momentums[best])
+# #     rets = np.log(stocks[best].iloc[end - 90:end])
+# #     x = np.arange(len(rets))
+# #     slope, intercept , r_value , p_value , std_err = linregress(x, rets)
+# #     plt.plot(np.arange(180), stocks[best][end-90:end+90])
+# #     plt.plot(x,np.e ** (intercept + slope * x))
